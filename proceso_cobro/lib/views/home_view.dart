c60bad4e04c81cf02512d7f27c1c790095644b43
Alex-mdx <75091176+Alex-mdx@users.noreply.github.com>
2023-05-03 16:41:38 -0600

    creacion de combo box

--- proceso_cobro/lib/views/home_view.dart
+++ proceso_cobro/lib/views/home_view.dart
@@ -28,17 +28,30 @@ class HomeContenedor extends StatefulWidget {
 }
 
 class _HomeContenedorState extends State<HomeContenedor> {
-  @override
   List<String> elementos = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+  List<String> chips = ['yucatan', 'merida', 'espita', 'tizimin', 'temozon'];
+  @override
   Widget build(BuildContext context) {
     final ProCosto = Provider.of<ProvCosto>(context);
+
+    List<Chip> chipsList = chips
+        .map((chip) => Chip(
+              backgroundColor: Color.fromARGB(255, 141, 219, 255),
+              elevation: 2,
+              label: Text(chip,
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.035)),
+            ))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Rutas de contacto',
           style: TextStyle(
             color: Colors.white, // Color del texto
-            fontSize: 20.0, // Tamaño del texto
+            fontSize:
+                MediaQuery.of(context).size.width * 0.05, // Tamaño del texto
             fontWeight: FontWeight.bold, // Grosor del texto
           ),
         ),
@@ -54,24 +67,28 @@ class _HomeContenedorState extends State<HomeContenedor> {
           ),
         ),
       ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: elementos.length,
-          itemBuilder: (context, index) {
-            return ExpansionTile(
-              title: Text(
-                elementos[index],
-                style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width * 0.05,
-                ),
+      body: ListView.builder(
+        itemCount: elementos.length,
+        itemBuilder: (context, index) {
+          return ExpansionTile(
+            title: Text(
+              elementos[index],
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
               ),
-              children: <Widget>[
-                // Aquí puedes añadir cualquier widget que quieras que se muestre cuando se expanda el elemento
-                Text("Contenido del elemento"),
-              ],
-            );
-          },
-        ),
+            ),
+            children: <Widget>[
+              SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Wrap(
+                  spacing: MediaQuery.of(context).size.width * 0.01,
+                  children: chipsList,
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
