f6267f697bbf8891cceae2a9c67f6f9870e0db79
Alex-mdx <75091176+Alex-mdx@users.noreply.github.com>
2023-05-03 15:21:28 -0600

    homview

---
+++ proceso_cobro/lib/provider/provider_costo.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class ProvCosto with ChangeNotifier {
+
+}
---
+++ proceso_cobro/lib/views/home_view.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:proceso_cobro/provider/provider_costo.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView({super.key});
+  final ProvCosto ProCosto = ProvCosto();
+  @override
+  State<HomeView> createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+  final ProvCosto ProCosto = ProvCosto();
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => new ProvCosto(),
+      child: Scaffold(body: HomeContenedor()),
+    );
+  }
+}
+
+class HomeContenedor extends StatefulWidget {
+  @override
+  _HomeContenedorState createState() => _HomeContenedorState();
+}
+
+class _HomeContenedorState extends State<HomeContenedor> {
+  @override
+  List<String> elementos = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+  Widget build(BuildContext context) {
+    final ProCosto = Provider.of<ProvCosto>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Rutas de contacto',
+          style: TextStyle(
+            color: Colors.white, // Color del texto
+            fontSize: 20.0, // Tamaño del texto
+            fontWeight: FontWeight.bold, // Grosor del texto
+          ),
+        ),
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            // Código para cerrar la aplicación
+            SystemNavigator.pop();
+          },
+          child: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: elementos.length,
+          itemBuilder: (context, index) {
+            return ExpansionTile(
+              title: Text(
+                elementos[index],
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                ),
+              ),
+              children: <Widget>[
+                // Aquí puedes añadir cualquier widget que quieras que se muestre cuando se expanda el elemento
+                Text("Contenido del elemento"),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
